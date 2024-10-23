d41222fac27729d092a91f112146f2a029053c8f
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-10-23 14:20:56 -0700

    🚧 Add player selector

--- app.R
+++ app.R
@@ -2,8 +2,11 @@ library(shiny)
 library(tidyverse)
 
 datos <- read_csv("www/data/datos_rpe_act2_c3.csv")
+all_playes <- datos$Name |> unique()
 ui <- bslib::page_sidebar(
-  sidebar = bslib::sidebar(),
+  sidebar = bslib::sidebar(
+    selectInput("name", "Nombre", all_playes, selected = "Matt"),
+  ),
   titlePanel(title = span(img(src = "logo.jpeg", height = 35), "")),
   tableOutput("static"),
   dataTableOutput("dynamic")
