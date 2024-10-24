186c30318fac467b9f8e6c79978e826b17980292
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-10-24 13:13:03 -0700

    🚧 Use `crosstalk` and `plotly` and `bslib`

--- app.R
+++ app.R
@@ -1,23 +1,43 @@
 library(shiny)
+library(crosstalk)
 library(tidyverse)
+library(plotly)
 
 datos <- read_csv("www/data/datos_rpe_act2_c3.csv", show_col_types = FALSE)
-all_playes <- datos$Name |> unique()
-ui <- bslib::page_sidebar(
-  sidebar = bslib::sidebar(
-    selectInput("name", "Nombre", all_playes, selected = "Matt"),
-  ),
-  titlePanel(title = span(img(src = "logo.jpeg", height = 35), "")),
-  DT::DTOutput("dynamic"),
-  plotOutput("plot", brush = "plot_brush")
+dat <- SharedData$new(datos)
+barplot <- SharedData$new(datos)
 
+sidebar_diamonds <- bslib::layout_sidebar(
+  sidebar = filter_select("name", "Nombre", dat, ~Name, allLevels = TRUE, multiple = FALSE),
+  plot_ly(dat, x = ~Date, y = ~Value, type = 'scatter') |>
+    layout(title = 'Valoración subjetiva del esfuerzo por jugador',
+         xaxis = list(title = ''),
+         yaxis = list(title = 'Esfuerzo'))
 )
-server <- function(input, output, session) {
-  output$plot <- renderPlot({
-    datos |>
-      filter(Name == input$name) |>
-      ggplot(aes(x=Date, Value)) + geom_point()
-  }, res = 96)
-  output$dynamic <- DT::renderDT(datos, options = list(pageLength = 5))
+
+sidebar_bar <- bslib::layout_sidebar(
+  sidebar = filter_select("date", "Fecha", barplot, ~Date, allLevels = FALSE, multiple = FALSE),
+  plot_ly(barplot) |>
+    add_histogram(x = ~Value) |>
+    layout(title = 'Valoración subjetiva del esfuerzo por día',
+         xaxis = list(title = 'Esfuerzo'),
+         yaxis = list(title = 'Frecuencia'))
+)
+
+
+ui <- bslib::page_fillable(
+  bslib::card(
+    full_screen = TRUE,
+    bslib::card_header("Respuesta por jugador"),
+    sidebar_diamonds
+  ),
+  bslib::card(
+    full_screen = TRUE,
+    bslib::card_header("Respuesta por día"),
+    sidebar_bar
+  )
+)
+
+server <- function(input, output) {
 }
 shinyApp(ui, server)
